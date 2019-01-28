eccc8d85d0ac95b593015f73b492beccc89136eb
zhoujing <zhou.yang.god.king@gmail.com>
2019-01-28 15:43:50 +0800

    c

--- web/viewer.js
+++ web/viewer.js
@@ -1,40 +1,4 @@
-/* Copyright 2016 Mozilla Foundation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/* globals chrome */
-
 'use strict';
-
-let pdfjsWebApp, pdfjsWebAppOptions;
-if (typeof PDFJSDev !== 'undefined' && PDFJSDev.test('PRODUCTION')) {
-  pdfjsWebApp = require('./app.js');
-  pdfjsWebAppOptions = require('./app_options.js');
-}
-
-if (typeof PDFJSDev !== 'undefined' && PDFJSDev.test('FIREFOX || MOZCENTRAL')) {
-  require('./firefoxcom.js');
-  require('./firefox_print_service.js');
-}
-if (typeof PDFJSDev !== 'undefined' && PDFJSDev.test('GENERIC')) {
-  require('./genericcom.js');
-}
-if (typeof PDFJSDev !== 'undefined' && PDFJSDev.test('CHROME')) {
-  require('./chromecom.js');
-}
-if (typeof PDFJSDev !== 'undefined' && PDFJSDev.test('CHROME || GENERIC')) {
-  require('./pdf_print_service.js');
-}
 
 function getViewerConfiguration() {
   return {
